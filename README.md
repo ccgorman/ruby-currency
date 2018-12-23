75bad4443d1104d6b0d2c736f02352281cf68fd3
Christopher Gorman <mail@chrisgorman.co.uk>
2018-12-23 19:43:41 +0000

    Add in validation

--- README.md
+++ README.md
@@ -8,8 +8,13 @@ Please see the INSTRUCTIONS.md file for more information.
 
 ## Your Solution Setup and Run Instructions
 
-Please include instructions on how to setup and run your solution here.
+1. Installed Ruby with rbenv
+2. Installed version 2.5.3
+3. Switched Mac to use this version $ rbenv local 2.5.3 - this was actually a blocker for a while to get RubyGems installed until I noticed my Mac was using an old version and figured out how to switch to the new version
+4. Set up a git repo so I could track my changes and roll back if required
 
 ## Your Design Decisions
+1. Decided to put validation on user input, if a new source currency is added then the access to it ideally should be the same way, alternatively validation could be put on each source currency for user input
+
 
 We'd love to hear your thoughts around any design decisions you made while coding your solution.
--- lib/currency_exchange.rb
+++ lib/currency_exchange.rb
@@ -1,10 +1,33 @@
 class CurrencyExchange
 
+  require 'date'
+
   # Return the exchange rate between from_currency and to_currency on date as a float.
   # Raises an exception if unable to calculate requested rate.
   # Raises an exception if there is no rate for the date provided.
   def self.rate(date, from_currency, to_currency)
     # TODO: calculate and return rate
+    validation = self.validateInput(date, from_currency, to_currency)
+    if (validation[0])
+      puts("Continue to get currency")
+    else
+      puts(validation[1])
+    end
+  end
+
+  # Returns an array
+  # [0] => boolean true is valid false is not valid
+  # [1] => string information of state
+  def self.validateInput(date, from_currency, to_currency)
+  	if !date.instance_of?(Date)
+      return [false, "Date not valid"]
+    elsif !from_currency.instance_of?(String) || from_currency.length != 3
+      return [false, "From currency not valid"]
+    elsif !to_currency.instance_of?(String) || to_currency.length != 3
+      return [false, "To currency not valid"]
+    else
+      return [true, "Valid"]
+    end
   end
 
 end
